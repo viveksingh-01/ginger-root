346e75f41c68534cc19842d536c95791ba0739a6
Vivek Singh <viveksingh9301@gmail.com>
2026-01-08 15:22:09 +0530

    Create HTTP server using Gin and port from environment variables

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Welcome to ginger-root!")
+	log.Println("Welcome to Ginger API.")
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r := gin.Default()
+	r.Run(":" + port)
 }
