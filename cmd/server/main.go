f596f5806af60cfe07ce0add81e5eff822e1f4d2
Vivek Singh <viveksingh9301@gmail.com>
2026-01-09 08:35:33 +0530

    refactor: integrate configuration management for server and DB connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,32 +2,23 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+	"github.com/viveksingh-01/ginger-root/internal/config"
 	"github.com/viveksingh-01/ginger-root/internal/database"
 )
 
 func main() {
 	log.Println("Welcome to Ginger API.")
+	cfg, _ := config.Load()
 
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	database.Connect(os.Getenv("MONGODB_URI"))
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+	database.Connect(cfg.MongoURI)
 
 	gin.SetMode(gin.ReleaseMode)
 
 	r := gin.Default()
 	r.SetTrustedProxies(nil)
 
-	log.Println("Server started on port:", port)
-	r.Run(":" + port)
+	log.Println("Server started on port:", cfg.Port)
+	r.Run(":" + cfg.Port)
 }
