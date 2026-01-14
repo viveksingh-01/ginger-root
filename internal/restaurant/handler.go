b0d4560bf4232a936948ccac7d2e3f7fd06f429e
Vivek Singh <viveksingh9301@gmail.com>
2026-01-14 21:58:14 +0530

    feat: implement HTTP handler for restaurant requests

---
+++ internal/restaurant/handler.go
@@ -0,0 +1,51 @@
+package restaurant
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// This code:
+// * Handles incoming HTTP requests
+// * Calls the service layer to perform business logic
+// * Converts results into HTTP responses (JSON)
+// * Handles errors and HTTP status codes
+// This is the outermost layer of our application.
+
+// Handler depends on the Service
+// It delegates all business work to the service
+// It only handles HTTP concerns
+type Handler struct {
+	service *Service
+}
+
+// Creates a new handler
+// Injects the service dependency
+// Keeps handler easy to test and initialize
+func NewHandler(s *Service) *Handler {
+	return &Handler{service: s}
+}
+
+// This method is an HTTP endpoint (registered to a route)
+func (h *Handler) List(c *gin.Context) {
+	// c.Request.Context() extracts the standard Go context.Context
+	// This context:
+	// 1. Is cancelled if the client disconnects
+	// 2. Carries request-scoped deadlines
+	// The handler calls the service to fetch list of restaurants
+	restaurants, err := h.service.List(c.Request.Context())
+	if err != nil {
+		log.Println("Error occurred while fetching restaurants", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to fetch restaurants"})
+		return
+	}
+
+	// Sends HTTP 200 OK
+	// Serializes restaurants to JSON
+	// Gin automatically:
+	// * Sets Content-Type: application/json
+	// * Marshals Go structs into JSON
+	c.JSON(http.StatusOK, gin.H{"data": restaurants})
+}
