b88bc524f545791bda5b236eea2aa8d64b7852c3
Vivek Singh <viveksingh9301@gmail.com>
2026-02-04 21:43:26 +0530

    feat: implement menu repository for database interactions
    - create Repository struct to manage menu data in MongoDB
    - add NewRepository function to initialize the repository with the menus collection
    - implement FindByRestaurantID method to retrieve menu by restaurant ID

---
+++ internal/menu/respository.go
@@ -0,0 +1,25 @@
+package menu
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+type Repository struct {
+	collection *mongo.Collection
+}
+
+func NewRepository(db *mongo.Database) *Repository {
+	return &Repository{collection: db.Collection("menus")}
+}
+
+func (r *Repository) FindByRestaurantID(ctx context.Context, restaurantID string) (*Menu, error) {
+	var menu Menu
+	err := r.collection.FindOne(ctx, bson.M{"restaurantId": restaurantID}).Decode(&menu)
+	if err != nil {
+		return nil, err
+	}
+	return &menu, nil
+}
