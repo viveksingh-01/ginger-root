1b3b613706fee3e6d5e279553634fda64109a493
Vivek Singh <viveksingh9301@gmail.com>
2026-02-10 18:24:47 +0530

    feat: implement FindByID method in restaurant repository
    - add FindByID method to retrieve a restaurant by its ID from the database

--- internal/restaurant/repository.go
+++ internal/restaurant/repository.go
@@ -73,3 +73,12 @@ func (r *Repository) List(ctx context.Context, skip, limit int64, f Filter, sort
 
 	return restaurants, nil
 }
+
+func (r *Repository) FindByID(ctx context.Context, restaurantId string) (*Restaurant, error) {
+	var restaurant Restaurant
+	err := r.collection.FindOne(ctx, bson.M{"id": restaurantId}).Decode(&restaurant)
+	if err != nil {
+		return nil, err
+	}
+	return &restaurant, nil
+}
