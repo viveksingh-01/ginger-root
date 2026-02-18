0797b2b8889d6b48cb152b59bf8763096a4079a6
Vivek Singh <viveksingh9301@gmail.com>
2026-02-18 16:58:27 +0530

    feat: improve error handling in GetRestaurantMenu for restaurant not found
    - add specific handling for ErrRestaurantNotFound to return a 404 status
    - refactor variable naming for clarity in GetRestaurantMenu method

--- internal/restaurantmenu/handler.go
+++ internal/restaurantmenu/handler.go
@@ -1,9 +1,11 @@
 package restaurantmenu
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/viveksingh-01/ginger-root/internal/restaurant"
 )
 
 type Handler struct {
@@ -16,10 +18,14 @@ func NewHandler(s *Service) *Handler {
 
 func (h *Handler) GetRestaurantMenu(c *gin.Context) {
 	restaurantID := c.Param("restaurantId")
-	restaurantMenu, err := h.service.GetRestaurantMenu(c, restaurantID)
+	r, err := h.service.GetRestaurantMenu(c, restaurantID)
 	if err != nil {
+		if errors.Is(err, restaurant.ErrRestaurantNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, restaurantMenu)
+	c.JSON(http.StatusOK, r)
 }
