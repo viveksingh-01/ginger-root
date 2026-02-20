6113cd10e8618facdcadc11490b467c5d4f982da
Vivek Singh <viveksingh9301@gmail.com>
2026-02-20 19:12:04 +0530

    feat: add search route for restaurants
    - register new GET route for searching restaurants in routes.go

--- internal/restaurant/routes.go
+++ internal/restaurant/routes.go
@@ -5,4 +5,5 @@ import "github.com/gin-gonic/gin"
 func RegisterRoutes(rg *gin.RouterGroup, handler *Handler) {
 	rg.GET("/restaurants", handler.List)
 	rg.GET("/restaurants/:restaurantId", handler.GetRestaurant)
+	rg.GET("restaurants/search", handler.Search)
 }
