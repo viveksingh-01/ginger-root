0f76becbdb37a79a0ffa54bcd713d374ab56e92c
Vivek Singh <viveksingh9301@gmail.com>
2026-04-05 02:12:47 +0530

    feat: add JWT authentication support
    - implement GenerateToken function to create JWTs with user ID and expiration time

--- internal/auth/utils.go
+++ internal/auth/utils.go
@@ -1,6 +1,12 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
@@ -10,4 +16,15 @@ func HashPassword(password string) (string, error) {
 func CheckPassword(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
+
+func GenerateToken(userID string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"userId": userID,
+		"exp":    time.Now().Add(72 * time.Hour).Unix(),
+	})
+
+	return token.SignedString(jwtSecret)
 }
