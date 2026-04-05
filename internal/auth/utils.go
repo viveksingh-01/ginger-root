a1c015b365974ac070448665fe78e68ddbafc32c
Vivek Singh <viveksingh9301@gmail.com>
2026-04-06 01:03:42 +0530

    feat: enhance JWT token generation and validation
    - refactor GenerateToken function to use a structured Claims type with issued and expiration dates
    - implement ValidateToken function to parse and validate JWTs, returning claims or an error

--- internal/auth/utils.go
+++ internal/auth/utils.go
@@ -20,11 +20,39 @@ func CheckPassword(password, hash string) bool {
 
 var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
 
-func GenerateToken(userID string) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"userId": userID,
-		"exp":    time.Now().Add(72 * time.Hour).Unix(),
+type Claims struct {
+	UserID string `json:"userId"`
+	jwt.RegisteredClaims
+}
+
+// Creates JWT token
+func GenerateToken(userId string) (string, error) {
+	claims := Claims{
+		UserID: userId,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(72 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
+}
+
+// Parses and validates JWT
+func ValidateToken(tokenStr string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
 	})
 
-	return token.SignedString(jwtSecret)
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, err
+	}
+
+	return claims, nil
 }
