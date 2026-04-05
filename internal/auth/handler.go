cf5f090b366ed4b3f01437cd7534e6c02dd70922
Vivek Singh <viveksingh9301@gmail.com>
2026-04-06 00:44:19 +0530

    feat: implement login handler for user authentication
    - add Login method to handle user login requests
    - validate incoming JSON data and return appropriate error responses
    - authenticate users and return JWT token along with user details upon successful login

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -42,3 +42,33 @@ func (h *Handler) Signup(c *gin.Context) {
 		},
 	})
 }
+
+func (h *Handler) Login(c *gin.Context) {
+	var req *LoginRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, &Response{
+			StatusCode:    1,
+			StatusMessage: err.Error(),
+		})
+		return
+	}
+
+	user, token, err := h.service.Login(c, req)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, &Response{
+			StatusCode:    1,
+			StatusMessage: err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, &Response{
+		StatusCode:    0,
+		StatusMessage: "done successfully",
+		Data: &AuthResponse{
+			User:  ToUserResponse(user),
+			Token: token,
+		},
+	})
+}
