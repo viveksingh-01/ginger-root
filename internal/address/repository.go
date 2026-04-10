7d03c4efe0dea71952d07e585ba5cb539b3e81eb
Vivek Singh <viveksingh9301@gmail.com>
2026-04-10 22:59:02 +0530

    feat: implement address repository for MongoDB operations
    - create Repository struct to manage address data in MongoDB
    - add NewRepository function for initializing the repository
    - implement Create method for inserting new addresses
    - add GetByUserID method to retrieve addresses associated with a specific user

---
+++ internal/address/repository.go
@@ -0,0 +1,45 @@
+package address
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+type Repository struct {
+	collection *mongo.Collection
+}
+
+func NewRepository(db *mongo.Database) *Repository {
+	return &Repository{collection: db.Collection("addresses")}
+}
+
+func (r *Repository) Create(ctx context.Context, address *Address) (*Address, error) {
+	res, err := r.collection.InsertOne(ctx, address)
+	if err != nil {
+		return nil, err
+	}
+
+	address.ID = res.InsertedID.(bson.ObjectID)
+	return address, nil
+}
+
+func (r *Repository) GetByUserID(ctx context.Context, userId bson.ObjectID) ([]*Address, error) {
+	cursor, err := r.collection.Find(ctx, bson.M{"userId": userId})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var addresses []*Address
+	for cursor.Next(ctx) {
+		var addr Address
+		if err := cursor.Decode(&addr); err != nil {
+			return nil, err
+		}
+		addresses = append(addresses, &addr)
+	}
+
+	return addresses, nil
+}
