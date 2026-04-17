34a68b578f6245d63770496c5f065b84e2ea2873
Vivek Singh <viveksingh9301@gmail.com>
2026-04-17 17:10:21 +0530

    feat: extend Cart model with timestamps and BSON support
    - update Cart ID type to bson.ObjectID for MongoDB compatibility
    - add CreatedAt and UpdatedAt fields to track cart creation and modification times

--- internal/cart/model.go
+++ internal/cart/model.go
@@ -1,15 +1,23 @@
 package cart
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
 type CartItem struct {
 	MenuItemID string `bson:"menuItemId"`
 	Quantity   int    `bson:"quantity"`
 }
 
 type Cart struct {
-	ID           string     `bson:"_id,omitempty"`
-	UserID       string     `bson:"userId,omitempty"`
-	GuestID      string     `bson:"guestId,omitempty"`
-	RestaurantID string     `bson:"restaurantId"`
-	AddressID    string     `bson:"addressId"`
-	Items        []CartItem `bson:"cartItems"`
+	ID           bson.ObjectID `bson:"_id,omitempty"`
+	UserID       string        `bson:"userId,omitempty"`
+	GuestID      string        `bson:"guestId,omitempty"`
+	RestaurantID string        `bson:"restaurantId"`
+	AddressID    string        `bson:"addressId"`
+	Items        []CartItem    `bson:"cartItems"`
+	CreatedAt    time.Time     `bson:"createdAt"`
+	UpdatedAt    time.Time     `bson:"updatedAt"`
 }
